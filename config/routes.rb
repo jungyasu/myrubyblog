e24832b000cb602b0c48c7b6a7cc37b961bbe871
jungyoseok <yasu3237@gmail.com>
2016-04-11 17:55:24 -0700

    heroku update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  # get '/posts' => 'posts'
+
+    resources :posts
 
 
   # The priority is based upon order of creation: first created -> highest priority.
